dcb0e57d6ce8124b955a761c6ec2f3494a633ae9
Leroy <ntruderleroy@gmail.com>
2024-08-03 17:36:50 +0300

    commit

---
+++ src/views/OurStory.vue
@@ -0,0 +1,88 @@
+<template>
+	<main class="story">
+		<header>
+			<div class="header__wrapper">
+				<h1 class="header__head">Meet The founders</h1>
+			</div>
+		</header>
+		<div class="founders">
+			<div class="founder1">
+				<div class="founder1__textsi">
+					<h2 class="founder1__head">Mama Dolfine</h2>
+					<span class="founder1__title">
+						Founder, Korando Children Faith Foundation
+					</span>
+					<p class="founder1__story">
+						Mama Dolfine was a government-employed teacher for 20
+						years until she lost her 14 and 27 year old sons in 1996
+						within just one year. This very traumatizing experience
+						brought her to depression followed by a premature
+						retirement. As she was staying at home, she started
+						taking care of the first five orphans Miskela, Lucy,
+						Isiah, Brenda and Kennedy; that gave a new meaning to
+						her life. More of them started coming, finding shelter
+						and care with her, but also food and financial support
+						to be send to public school. In 2006 she opened her own
+						non-formal school to accommodate even more children and
+						reduce the increasing cost of school fees. Her husband
+						Patrick became the headteacher of the new school and
+						supported her greatly throughout the years. Then in 2014
+						she had to handle another huge loss when her husband
+						Patrick passed away. Though life left it’s scars, it
+						didn’t stop her. And today, at 69 years old, she keeps
+						on struggling for those children in need and must have
+						changed over 1000 lives already.
+					</p>
+				</div>
+				<div class="founder1__imagesi">
+					<img src="" alt="" />
+				</div>
+			</div>
+			<div class="founder2">
+				<div class="founder2__imagesi">
+					<img src="" alt="" />
+				</div>
+				<div class="founder2__textsi">
+					<h2 class="founder2__head">Toto</h2>
+					<span class="founder2__title">
+						Founder, Better Me e.V.
+					</span>
+					<p class="founder2__story">
+						Toto, a well travelled globetrotter and visionary
+						entrepreneur, had discovered the Korando Educational
+						Center, near Kisumu, Kenya, in 2012 during his first
+						journey through eastern and southern Africa. By absolute
+						coincidence (or destiny) he met Mama Dolfine, a Kenyan
+						woman who, at 64, keeps on giving all her time and
+						energy to provide food, shelter, healthcare and
+						education to the orphans, vulnerable children and widows
+						of her local community. Her devoted commitment touched
+						his heart and he was able to raise small funds
+						immediately to build a new classroom, showers, buy two
+						dozen chickens and a computer. His time was limited and
+						so he had to leave after just 7 days. But Mama and the
+						children stayed in his heart and he promised himself to
+						return. And so he did. In January 2015 he made his
+						promise come true. Together with his good friend
+						Mathilde he started a long 5200km journey from South
+						Africa to Kenya, crossing Zimbabwe, Zambia, Malawi,
+						Tanzania and Uganda on their way, to raise awareness but
+						also funds for the Korando Educational Center. What
+						started as a simple idea to help Mama Dolfine and her
+						children, turned into a big vision then called Cheap
+						Trip Big Impact. Later the vision evolved into the
+						Better Me Foundation and Torsten, his team and the
+						volunteers are working tirelessly to turn its
+						ever-expanding vision into reality. This transparent and
+						rapidly growing grassroots organization is aiming for
+						nothing less than the stars and you can be part of this
+						journey!
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="block-img">
+			<img src="" alt="" />
+		</div>
+	</main>
+</template>
